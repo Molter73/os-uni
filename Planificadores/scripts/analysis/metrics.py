6ce9f0f93f0c21da24752ee2c5e547cb2b821fba
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-04-02 12:54:59 +0200

    Ajustado cálculo de métricas a la salida del planificador

--- Planificadores/scripts/analysis/metrics.py
+++ Planificadores/scripts/analysis/metrics.py
@@ -17,55 +17,105 @@ def procesar_datos(df):
     df['Tiempo de Respuesta'] = df['Tiempo de Espera']
 
 
-# Función para calcular métricas de rendimiento
-def calcular_metricas(df, algorithm, input_type, preemptive=False):
+def calcular_metricas_preemptive(df, algorithm, input_type):
     metrics = {
         'algorithm': algorithm,
         'input_type': input_type,
     }
-    # Tiempo de espera promedio: Tiempo que cada proceso pasa en la cola antes
-    # de su ejecución. Es una métrica crucial para evaluar la percepción de
-    # rapidez de un sistema por parte del usuario.
-    metrics['tiempo_espera_promedio'] = df['Tiempo de Espera'].mean()
+
+    # Ordenar el dataframe por 'ID Proceso' y luego por 'Inicio de
+    # Ejecución'
+    df_sorted = df.sort_values(by=['ID Proceso', 'Inicio de Ejecución'])
+
+    # Eliminar duplicados manteniendo la primera aparición para obtener el
+    # inicio de ejecución de cada proceso
+    df_first_execution = df_sorted.copy().drop_duplicates(
+        subset='ID Proceso', keep='first')
+    df_last_execution = df_sorted.drop_duplicates(
+        subset='ID Proceso', keep='last')
+
+    df_first_execution.loc[:, 'Tiempo de Respuesta'] = \
+        df_first_execution['Inicio de Ejecución'] - \
+        df_first_execution['Tiempo de Llegada']
+    df_first_execution.drop('Tiempo de Retorno', axis=1)
+    df_first_execution.loc['Tiempo de Retorno'] = \
+        df_last_execution['Fin de Ejecución'] - \
+        df_first_execution['Tiempo de Llegada']
+
+    metrics['tiempo_respuesta_promedio'] = \
+        df_first_execution['Tiempo de Respuesta'].mean()
+
+    metrics['tiempo_espera_promedio'] = \
+        df_first_execution['Tiempo de Espera'].mean()
+
+    metrics['var_tiempo_espera'] = \
+        df_first_execution['Tiempo de Espera'].std()
+
+    metrics['max_tiempo_espera'] = int(
+        df_first_execution['Tiempo de Espera'].max())
+
+    metrics['tiempo_retorno_promedio'] = (
+        df_last_execution['Fin de Ejecución'] -
+        df_first_execution['Tiempo de Llegada']).mean()
+
+    # El tiempo total observado es el periodo desde el inicio de la primera
+    # ejecución hasta el final de la última ejecución.
+    tiempo_total_observado = \
+        df_last_execution['Fin de Ejecución'].max() - \
+        df_first_execution['Tiempo de Llegada'].min()
+
+    tiempo_total_actividad_cpu = \
+        df_last_execution['Fin de Ejecución'].max() - \
+        df_first_execution['Inicio de Ejecución'].min()
+
+    # Utilización de la CPU: Porcentaje del tiempo de actividad de la CPU sobre
+    # el tiempo total observado.
+    metrics['utilizacion_cpu'] = \
+        (tiempo_total_actividad_cpu / tiempo_total_observado) * 100
+
+    # Rendimiento: Número de procesos completados por unidad de tiempo.
+    # Esta métrica ayuda a entender cuánto trabajo es capaz de realizar el
+    # sistema en un tiempo determinado.
+    metrics['rendimiento'] = len(df_first_execution) / tiempo_total_observado
+
+    return metrics
+
+
+# Función para calcular métricas de rendimiento
+def calcular_metricas(df, algorithm, input_type, preemptive):
+    if preemptive:
+        return calcular_metricas_preemptive(df, algorithm, input_type)
+
+    metrics = {
+        'algorithm': algorithm,
+        'input_type': input_type,
+    }
 
     # Tiempo de retorno promedio: Tiempo total desde la llegada del proceso
-    # hasta su finalización. Importante para entender el tiempo total que un
-    # proceso requiere para completarse, incluyendo el tiempo de espera y de
-    # ejecución.
+    # hasta su finalización. Importante para entender el tiempo total que
+    # un proceso requiere para completarse, incluyendo el tiempo de espera
+    # y de ejecución.
     metrics['tiempo_retorno_promedio'] = df['Tiempo de Retorno'].mean()
+
+    # Tiempo de espera promedio: Tiempo que cada proceso pasa en la cola
+    # antes de su ejecución. Es una métrica crucial para evaluar la
+    # percepción de rapidez de un sistema por parte del usuario.
+    metrics['tiempo_espera_promedio'] = df['Tiempo de Espera'].mean()
 
     # Tiempo de respuesta promedio: Tiempo desde que el proceso llega hasta
     # que se inicia su ejecución. En este caso, se asume igual al tiempo de
-    # espera para simplificación, pero en algoritmos con desalojo podría ser
-    # diferente.
-    if preemptive:
-        # Ordenar el dataframe por 'ID Proceso' y luego por 'Inicio de
-        # Ejecución'
-        df_sorted = df.sort_values(by=['ID Proceso', 'Inicio de Ejecución'])
-        # Eliminar duplicados manteniendo la primera aparición para obtener el
-        # inicio de ejecución de cada proceso
-        df_first_execution = df_sorted.drop_duplicates(
-            subset='ID Proceso', keep='first')
-        # Calcular el tiempo de respuesta como la diferencia entre 'Inicio de
-        # Ejecución' y 'Tiempo de Llegada'
-        df_first_execution.loc[:, 'Tiempo de Respuesta'] = \
-            df_first_execution['Inicio de Ejecución'] - \
-            df_first_execution['Tiempo de Llegada']
-        # Calcular el tiempo de respuesta promedio específicamente para
-        # algoritmos con prevaciado.
-        metrics['tiempo_respuesta_promedio'] = \
-            df_first_execution['Tiempo de Respuesta'].mean()
-    else:
-        metrics['tiempo_respuesta_promedio'] = df['Tiempo de Respuesta'].mean()
+    # espera para simplificación, pero en algoritmos con desalojo podría
+    # ser diferente.
+    metrics['tiempo_respuesta_promedio'] = df['Tiempo de Respuesta'].mean()
 
-    # Variabilidad del tiempo de espera: Mide cuánto varía el tiempo de espera
-    # entre diferentes procesos. Una alta variabilidad puede indicar una falta
-    # de equidad en el tratamiento de procesos.
+    # Variabilidad del tiempo de espera: Mide cuánto varía el tiempo de
+    # espera entre diferentes procesos. Una alta variabilidad puede indicar
+    # una falta de equidad en el tratamiento de procesos.
     metrics['var_tiempo_espera'] = df['Tiempo de Espera'].std()
 
-    # Máximo tiempo de espera: El mayor tiempo que un proceso ha esperado antes
-    # de ejecutarse. Útil para identificar casos extremos y evaluar la equidad
-    # del algoritmo.
+    # Máximo tiempo de espera: El mayor tiempo que un proceso ha esperado
+    # antes de ejecutarse. Útil para identificar casos extremos y evaluar
+    # la equidad del algoritmo.
     metrics['max_tiempo_espera'] = int(df['Tiempo de Espera'].max())
 
     # Utilización de la CPU: Porcentaje del tiempo total que la CPU estuvo
@@ -78,17 +128,10 @@ def calcular_metricas(df, algorithm, input_type, preemptive=False):
     tiempo_total_observado = \
         df['Fin de Ejecución'].max() - df['Tiempo de Llegada'].min()
 
-    if preemptive:
-        # Para Round Robin, consideramos cada periodo de ejecución
-        # individualmente. Sumamos solo las duraciones efectivas donde la CPU
-        # estuvo ejecutando procesos.
-        tiempo_total_actividad_cpu = \
-            df['Fin de Ejecución'].max() - df['Inicio de Ejecución'].min()
-    else:
-        # En otros algoritmos, si asumimos una ejecución continua sin grandes
-        # periodos de inactividad, podemos simplificar el cálculo a la suma de
-        # las duraciones de ráfaga.
-        tiempo_total_actividad_cpu = df['Duración de la Ráfaga'].sum()
+    # En otros algoritmos, si asumimos una ejecución continua sin grandes
+    # periodos de inactividad, podemos simplificar el cálculo a la suma de
+    # las duraciones de ráfaga.
+    tiempo_total_actividad_cpu = df['Duración de la Ráfaga'].sum()
 
     # Utilización de la CPU: Porcentaje del tiempo de actividad de la CPU sobre
     # el tiempo total observado.
@@ -123,7 +166,7 @@ def imprimir_metricas(metrics):
 def main(file, output, algorithm, input_type):
     df = pd.read_csv(file)
     procesar_datos(df)
-    preemptive = algorithm == 'rr' or algorithm == 'prioridad_aging'
+    preemptive = algorithm == 'rr' or algorithm == 'prioridad_preemptive'
     metrics = calcular_metricas(df, algorithm, input_type, preemptive)
 
     if output is None:
